b229ab6dcfe80e6037df3c704e86e5b3c55e497e
Seonggyu Park <seonggyu.park@brc.tamus.edu>
2024-01-11 15:08:08 -0600

    checked duplicated

--- pyfolder/temp_.py
+++ pyfolder/temp_.py
@@ -22,17 +22,18 @@ def read_output_rch_data(wd, colNum=6):
     )
 
 if __name__ == "__main__":
-    wd = "/Users/seonggyu.park/Documents/projects/kokshila/analysis/koksilah_swatmf/SWAT-MODFLOW"
+    # wd = "/Users/seonggyu.park/Documents/projects/kokshila/analysis/koksilah_swatmf/SWAT-MODFLOW"
+    wd = "D:/Projects/Watersheds/Koksilah/analysis/koksilah_swatmf/SWAT-MODFLOW"
     # outfd = "d:/Projects/Watersheds/Okavango/Analysis/2nd_cali"
     # get_rech_avg_m_df(wd).to_csv(os.path.join(outfd, 'test.csv'))
-    obd_file = "stf_day.obd.csv"
+    obd_file = "dtw_day.obd.csv"
     startDate = "1/1/2009"
     strObd = read_stf_obd(wd, obd_file)
-    output_rch = read_output_rch_data(wd)
-    # try:
-    df = output_rch.loc[30]
-    df.index = pd.date_range(startDate, periods=len(df.stf_sim))
-    # df2 = pd.concat([df, strObd["sub03"]], axis=1)
+    # output_rch = read_output_rch_data(wd)
+    # # try:
+    # df = output_rch.loc[30]
+    # df.index = pd.date_range(startDate, periods=len(df.stf_sim))
+    # # df2 = pd.concat([df, strObd["sub03"]], axis=1)
 
 
 
